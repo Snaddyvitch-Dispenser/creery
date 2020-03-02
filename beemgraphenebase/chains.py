cdc9486a28ca1eedf37de2926e8fffbc551f0ca7
Conor <28988626+Snaddyvitch-Dispenser@users.noreply.github.com>
2020-03-02 10:09:32 +0000

    Update chains.py

--- beemgraphenebase/chains.py
+++ beemgraphenebase/chains.py
@@ -9,10 +9,10 @@ known_chains = {
         "min_version": '0.0.0',
         "prefix": "CREA",
         "chain_assets": [
-            {"asset": "CBD", "symbol": "CBD", "precision": 3, "id": 0},
-            {"asset": "CREA", "symbol": "CREA", "precision": 3, "id": 1},
-            {"asset": "VESTS", "symbol": "VESTS", "precision": 6, "id": 2},
-            {"asset": "CGY", "symbol": "CGY", "precision": 5, "id": 3},
+            {"asset": "@@000000013", "symbol": "CBD", "precision": 3, "id": 0},
+            {"asset": "@@000000021", "symbol": "CREA", "precision": 3, "id": 1},
+            {"asset": "@@000000037", "symbol": "VESTS", "precision": 6, "id": 2},
+            {"asset": "@@000000005", "symbol": "CGY", "precision": 5, "id": 3},
         ],
     },
 }
